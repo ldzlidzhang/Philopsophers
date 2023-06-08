d0f02a56394609131a0219a1361a8216890616ea
Lidan Zhang <116870623+ldzlidzhang@users.noreply.github.com>
2023-06-08 10:14:13 +0300

    Add files via upload

---
+++ philo/philo.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lidanzhang <lidanzhang@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/08 08:33:43 by lidanzhang        #+#    #+#             */
+/*   Updated: 2023/06/08 08:36:44 by lidanzhang       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+
+typedef struct s_dining_table
+{
+	pthread_mutex_t	*forks;
+	int				*philosophers_eaten;
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_times_each_philosopher_must_eat;
+}			t_dining_table;
+
+#endif
